d88080da699906576df33e7aa1b3a922eea7c42d
Warit Phetkaeo <6810110318@psu.ac.th>
2026-02-19 18:54:29 +0700

    refactor(backend): update app and auth services logic

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -2,7 +2,7 @@
 import { Injectable, BadRequestException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { CreateUserDto } from './dto/create-user.dto'; // ✅ Import DTO
 
 @Injectable()
