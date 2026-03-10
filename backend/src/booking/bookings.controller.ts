73b40a0f4a62e4089cdd69eca76da7cfd613ceff
Peerapat Vannabavorn <6810110236@psu.ac.th>
2026-03-10 18:58:18 +0700

    secure booking endpoints with JWT AuthGuard

--- backend/src/booking/bookings.controller.ts
+++ backend/src/booking/bookings.controller.ts
@@ -1,6 +1,9 @@
-import { Controller, Get, Patch, Param, Body, Post, Delete } from '@nestjs/common';
+import { Controller, Get, Patch, Param, Body, Post, Delete, UseGuards } from '@nestjs/common';
 import { BookingsService } from './bookings.service';
+import { AuthGuard } from '@nestjs/passport';
 
+
+@UseGuards(AuthGuard('jwt'))
 @Controller('bookings')
 export class BookingsController {
   constructor(private readonly bookingsService: BookingsService) {}
